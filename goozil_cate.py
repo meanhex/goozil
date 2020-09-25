72afaffe16c524c5af59de05179e760e98bafd04
meanhex <meanhex7@gmail.com>
2020-09-25 23:26:26 +0900

    update searcher

--- goozil_cate.py
+++ goozil_cate.py
@@ -1,13 +1,31 @@
-# ver proto4
+# ver proto5
+# 상하단에서 상품을 추가로 표시하는 사이트는 제외
+# 카테고리가 주석없이 이미지로만 표시되는 경우 제외
+# tag search :      1. 파라메터 KEY를 포함하는 href를 가진 a tag를 검색
+#                   2. 중복이 발생할 경우, 깊이가 보다 상위 태그(부모로부터 가까운) 태그를 선택
+#                   3. 깊이가 같을 경우, 보다 많은 형제를 가진 태그를 선택
+#                   4. 형제가 같을 경우, 부모태그를 선택한 후 깊이를 -1로 표시
+# catagory search : 1. 태그 자체 정보 -> 자식 태그 정보 -> 형제 태그 정보 순으로 검색
+# page search :     1. 카테고리 url로 접근
+#                   2. PAGE_KEY와 매치되는 a tag를 모두 수집
+#                   3. tag내 href속성의 특정 지점을 추출
+#                   4. min page ~ max page로 설정함
+# item search :     1. 카테고리에서 검색된 page url으로 접근,
+#                   2. ITEM_KEY와 매치되는 a tag를 모두 수집,
+#                   3. tag의 자식, 형제 모두중 SOLDOUT_KEY를 포함하는 경우 수집 안함
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import time
 
+CATE_KEY = {re.compile(r'.list[.]+\w+[?].'),    #
+            }
+TAG_KEY = {'a', 'img'}                          # 
 PAGE_KEY = '&page='                             # page로 추정되는 위치의 시작          
 ITEM_KEY = {re.compile('.product_no=.'),        # item로 추정되는 위치의 시작
-            re.compile('./goods/.')}
+            re.compile('./goods/.'),
+            re.compile('.[?]index_no=.')}
 SOLDOUT_KEY = {'soldout', 'sellout', '품절'}
 # ▼
 class Shop :
@@ -33,72 +51,15 @@ def get_url(xshop_url, xurl) :
     else :
         return xshop_url + xurl
 
-# ▼ shop_list, tag_keys, cate_keys를 인자로 shop의 카테로리로 추정되는 link목록을 추가한 shop_list를 반환
-def get_cate(xshop, xtag_keys, xcate_keys) :
-    bs = BeautifulSoup(urlopen(xshop.url), "html.parser")
-    body = bs.find('body')
-    xshop.title = bs.head.title.string # shop class에 title 등록
-
-    cates = list()
-    cate_tags = get_tag_parent(xtag_keys, body, xcate_keys)
-    for _tag in cate_tags : # tag 속성에 text가 있는 경우(0), alt 속성이 있을 경우(1), (2), 같다면 부모를 저장함(3)
-        select_name = ''
-        select_href = _tag.get('tag').get('href')
-        if len(_tag.get('tag').text) > 0 : # (0)
-            text_tag = _tag.get('tag').text.replace('\n', '') # 줄바꿈 제외한 text 속성
-            if len(text_tag) > 0 : # 줄바꿈을 제외한 text 값이 존재할때(0)
-                select_name = text_tag
-        elif 'alt' in _tag.get('tag').attrs : # (1)
-            alt_tag = _tag.get('tag').attrs.get('alt').replace('\n', '') # 줄바꿈 제외한 alt 속성
-            if len(alt_tag) > 0 : # 줄바꿈을 제외한 alt 값이 존재할때(1)
-                select_name = alt_tag
-        else :
-            for _next in _tag.get('tag').next_siblings :
-                if str(type(_next)) == "<class 'bs4.element.Tag'>" :
-                    if len(_next.text) > 0 : 
-                        text_tag = _next.text.replace('\n', '')
-                        if len(text_tag) > 0 : 
-                            select_name = text_tag
-                            break
-                    elif 'alt' in _next.attrs : 
-                        alt_tag = _next.attrs.get('alt').replace('\n', '')
-                        if len(alt_tag) > 0 : 
-                            select_name = alt_tag
-                            break
-        if '&sort' in select_href :
-            select_href =  select_href.split('&sort')[0]
-        cates.append({'name' : select_name, 'href' : select_href, 'items' : None})
-    xshop.setCates(cates)
-
-    # cates = list()
-    # href_results = list() # result for tag with href
-    
-    # # 검색 시작
-    # href_results = body.find_all(xtag_keys, href=xcate_keys) # search to tag with href
-    # for result in href_results :
-    #     if len(result.text) > 2 : # 검색 결과중 '\n\n'도 포함되어 조건에 추가
-    #         cates.append({'name' : result.text.replace('\n', ''), 'href' : result.get('href'), 'items' : None})
-    #     elif 'alt' in result.attrs and len(result.attrs['alt']) > 2 :
-    #         cates.append({'name' : result.attrs['alt'].replace('\n', ''), 'href' : result.get('href'), 'items' : None})
-    #     elif result.next != None and type(result) == type(result.next) : #
-    #         if len(result.next.text) > 2 :
-    #             cates.append({'name' : result.next.text.replace('\n', ''), 'href' : result.get('href'), 'items' : None})
-    #         elif 'alt' in result.next.attrs and len(result.next.attrs['alt']) > 0 :
-    #             cates.append({'name' : result.next.attrs['alt'].replace('\n', ''), 'href' : result.get('href'), 'items' : None})
-    # # 검색 완료
-    # if len(cates) > 2 : # 검색결과가 2개 이상 있을때
-    #     cates = list({x.get('href') : x for x in cates}.values()) # href 기준으로 중복을 제거
-    #     xshop.setCates(cates) # for문으로 가져온 현재 xshop의 cates에 저장
-    # else : 
-    #     print('cate length is short than 3') # 검색 결과가 1개 이하일땐 저장하지 않음 : 카테고리가 1개 이하인 쇼핑몰은 없다고 가정
-    return
-
 def get_bodytag(xurl) :
     body = None
     while True :
         try :
             body = BeautifulSoup(urlopen(xurl), "html.parser").find('body') # url의 body를 반환
-            break
+            if body != None :
+                break
+            else :
+                print('# get body error')
         except :
             print('# get body error')
             continue
@@ -108,7 +69,7 @@ def get_bodytag(xurl) :
 def get_tag(xtag, xbs, xkey) :
     return xbs.find_all(xtag, href=xkey) # href속성에 key가 존재하는 tag를 반환
 
-def get_tag_parent(xtag, xbs, xkey) :   # dict{'tag' : 검색된 tag정보, 'deep' : 깊이 정보}
+def get_tag_deep(xtag, xbs, xkey) :   # dict{'tag' : 검색된 tag정보, 'deep' : 깊이 정보}
                                         # (deep : -1    = 최종 예외되어 부모가 저장됨)
                                         # (deep : 0     = 중복이 없어 깊이 정보가 필요없는 tag)
     tags = get_tag(xtag, xbs, xkey)
@@ -156,8 +117,6 @@ def get_tag_parent(xtag, xbs, xkey) :   # dict{'tag' : 검색된 tag정보, 'dee
 
 def get_page(xurl) :
     body = get_bodytag(xurl)
-    if body == None :
-        return 0, 0 # body를 못받아오는 에러에 대한 exception
     page_tags = get_tag('a', body, re.compile('.' + PAGE_KEY + '.'))
     page_tags = list({x : x.get('href') for x in page_tags}.values()) # tag 속성중 href가 중복되지 않은 href값을 저장
     result = list()
@@ -169,15 +128,13 @@ def get_page(xurl) :
 
 def get_item(xurl, xlen) :
     body = get_bodytag(xurl)
-    if body == None :
-        return
 
     page_matchs = [body.prettify().count(x) for x in SOLDOUT_KEY] # body에서 품절키를 카운트
     if xlen != 0 and max(page_matchs) >= xlen : # 상품 최대 개수 이상일때 검색하지 않음
         return # 검색마다 최대 100개의 상품이 검색될때 100개의 품절키가 검색되면 페이지 내 모든 상품이 품절이라 가정
 
     items = list() # 판매 가능한 아이템 리스트
-    item_tags = get_tag_parent('a', body, ITEM_KEY)
+    item_tags = get_tag_deep('a', body, ITEM_KEY)
     for _tag in item_tags :
         result = _tag.get('tag').prettify() # 현재 태그 정보를 저장
         next_tag = _tag.get('tag').next_siblings # 다음 태그정보 포인트
@@ -190,8 +147,7 @@ def get_item(xurl, xlen) :
             if _tag.get('deep') >= 0 : # deep가 일반 일때
                 items.append(_tag.get('tag').get('href'))
             else : # deep가 부모를 가르킬때
-                a_tag = get_tag('a', _tag.get('tag'), ITEM_KEY) # a tag중 첫번째를 가져옴
-                items.append(a_tag[0].get('href'))
+                items.append(_tag.get('tag').find('a').get('href'))
                 # deep = -1 은 중복 tag들 중에서 보다 유효한 tag를 가리지 못한 경우임
                 # 따라서 부모에서 검색된 자식 a tag중 어느 tag를 가져와도 된다고 가정
 
@@ -202,13 +158,57 @@ def get_item(xurl, xlen) :
     #     if num_item.isdigit() and num_item not in (x for x in result) : # 추출한 상품 번호가 숫자이며, result의 원소들이 문자열을 포함하지 않을때
     #         result.append(_href) # 상품번호 중복이 없는 href만 저장
     return items
+
+# ▼ shop_list, tag_keys, cate_keys를 인자로 shop의 카테로리로 추정되는 link목록을 추가한 shop_list를 반환
+def start_search_cate(xshop) :
+    bs = BeautifulSoup(urlopen(xshop.url), "html.parser")
+    body = bs.find('body')
+    xshop.title = bs.head.title.string # shop class에 title 등록
+
+    cates = list()
+    cate_tags = get_tag_deep(TAG_KEY, body, CATE_KEY)
+    for _tag in cate_tags : # tag 속성에 text가 있는 경우(0), alt 속성이 있을 경우(1), (2), 같다면 부모를 저장함(3)
+        if _tag.get('deep') < 0 :
+            _tag['tag'] = _tag.get('tag').find('a')
+        select_href = _tag.get('tag').get('href') # 저장할 href
+        if '&sort' in select_href : # href에 불필요한 옵션이 있을때
+            select_href =  select_href.split('&sort')[0] # 옵션 제거
+
+        select_name = '' # 저장할 cate name
+        if len(_tag.get('tag').text.replace('\n', '')) > 0: # (0)
+            select_name = _tag.get('tag').text.replace('\n', '')
+        elif 'alt' in _tag.get('tag').attrs and len(_tag.get('tag').attrs.get('alt').replace('\n', '')) > 0 : # (1)
+            select_name = _tag.get('tag').attrs.get('alt').replace('\n', '')
+        else : # cate name을 찾을 수 없을때 
+            for _child in _tag.get('tag').children : # 자식 검색
+                    if str(type(_child)) == "<class 'bs4.element.Tag'>" :
+                        if len(_child.text.replace('\n', '')) > 0 : 
+                            select_name = _child.text.replace('\n', '')
+                            break
+                        elif 'alt' in _child.attrs and len(_child.attrs.get('alt').replace('\n', '')) > 0 : 
+                            select_name = _child.attrs.get('alt').replace('\n', '')
+                            break
+
+            if len(select_name) == 0 : # 자식 검색 결과가 없을때
+                for _next in _tag.get('tag').next_siblings : # 형제 검색
+                    if str(type(_next)) == "<class 'bs4.element.Tag'>" :
+                        if len(_next.text.replace('\n', '')) > 0 : 
+                            select_name = _next.text.replace('\n', '')
+                            break
+                        elif 'alt' in _next.attrs and len(_next.attrs.get('alt').replace('\n', '')) > 0 : 
+                            select_name = len(_next.attrs.get('alt').replace('\n', '')) > 0
+                            break
+                
+
+        if len(select_name) > 0 and len(select_href) > 0 :
+            cates.append({'name' : select_name, 'href' : select_href, 'items' : None})
+    xshop.setCates(cates)
+    return
     
-def get_fullitem(xshop) :
+def start_search_item(xshop) :
     for _cate in xshop.cates :
         cate_url = get_url(xshop.url, _cate.get('href'))
         print('cate %s result' % _cate.get('name'))
-        if 'NEW' in _cate.get('name') :
-            vvvvvadsf = 123
         pages = get_page(cate_url) # i번째 카테고리에서 페이지를 불러옴
         in_page = 0
 
@@ -231,24 +231,13 @@ if __name__ == '__main__' :
     shop_list.append(Shop("http://www.da-sara.com/"))
     shop_list.append(Shop("http://www.swingingseoul.com/"))
     shop_list.append(Shop("http://www.smoothie-star.com/"))
-    shop_list.append(Shop("http://www.melodystyle.co.kr/"))
+    # shop_list.append(Shop("http://www.melodystyle.co.kr/")) # 개같네. 페이지마다 광고 상품 존나 띄워놔서 분간하기 어려움. 좆같게 브랜드관 따로 쳐 차려놓는것도 맘에안들어 썅
     shop_list.append(Shop("http://www.drvtg.co.kr/")) # 카테고리에 브랜드 카테고리까지 있음
-    shop_list.append(Shop("http://gujenara.co.kr/")) # 주석이나 카테고리 텍스트 없이 이미지로만 된 사이트
-    shop_list.append(Shop("http://regarment.com/"))
-
-    tag_keys = list() # tag keyword list : tag for category
-    tag_keys.append('a')
-    tag_keys.append('img')
-
-    cate_words = list() # category keyword list : category for used item
-    cate_words.append(r'.list[.]+\w+[?].') # ~list.□?~
-    cate_keys = [re.compile(x) for x in cate_words] # 정규식 추가
-
-    
+    # shop_list.append(Shop("http://gujenara.co.kr/")) # 주석이나 카테고리 텍스트 없이 이미지로만 된 사이트
     
     for _shop in shop_list :
-        get_cate(_shop, tag_keys, cate_keys)
+        start_search_cate(_shop)
         _shop.show()
-        # get_fullitem(_shop)
+        start_search_item(_shop)
     
     print('done')
